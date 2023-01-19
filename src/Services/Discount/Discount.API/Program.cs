18c5d18f1d7b103849e7794108fb4b39ff11ee9e
Trung VH <vhtrung52@gmail.com>
2023-01-19 14:14:04 +0700

    #05: add discount api with postgresql and docker compose

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,33 +1,29 @@
-using Catalog.API.Data;
-using Catalog.API.Repositories;
-using Microsoft.OpenApi.Models;
+using Discount.API.Extensions;
+using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
-});
-
-
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MigrateDatabase<Program>();
 
 app.Run();
